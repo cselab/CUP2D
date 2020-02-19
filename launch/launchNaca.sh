cf9edbdd64cffc92c7c13a60380116d1e2277e91
Pascal Weber <webepasc@student.ethz.ch>
2020-02-19 20:05:44 +0100

    NACA foil flapping

---
+++ launch/launchNaca.sh
@@ -0,0 +1,5 @@
+OPTIONS="-bpdx 16 -bpdy 8 -tdump 0.01 -nu 0.0045 -tend 10 -poissonType cosine -iterativePensalization 0"
+OBJECTS='NACA L=0.15 xpos=0.25 bFixed=1 bForced=1 xvel=0.3 Apitch=0.15 Fpitch=2.5
+'
+
+source launchCommon.sh
