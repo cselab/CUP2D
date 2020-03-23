316c95b0dd174e97a04ef2b58ae22707898a4441
Pascal Weber <webepasc@student.ethz.ch>
2020-03-23 09:01:47 +0100

    bugfix naca

--- launch/launchNaca.sh
+++ launch/launchNaca.sh
@@ -1,5 +1,5 @@
-OPTIONS="-bpdx 64 -bpdy 32 -tdump 0.1 -nu 0.000045 -tend 20 -poissonType cosine -iterativePensalization 0"
-OBJECTS='NACA L=0.15 xpos=0.25 bFixed=1 bForced=1 xvel=0.3 tAccel=5 Apitch=0.15 Fpitch=2.5
+OPTIONS="-bpdx 64 -bpdy 32 -tdump 0.05 -nu 0.000045 -tend 20 -poissonType cosine -iterativePensalization 0"
+OBJECTS='NACA L=0.15 xpos=0.25 bFixed=1 bForced=1 xvel=0.3 Apitch=0.15 Fpitch=1
 '
 
 source launchCommon.sh
--- launch/launchNacaStefan.sh
+++ launch/launchNacaStefan.sh
@@ -1,6 +1,6 @@
-OPTIONS="-bpdx 64 -bpdy 32 -tdump 0.1 -nu 0.000045 -tend 20 -poissonType cosine -iterativePensalization 0"
-OBJECTS='NACA L=0.15 xpos=0.25 bFixed=1 bForced=1 xvel=0.3 tAccel=5 Apitch=0.15 Fpitch=2.5
-stefanfish L=0.15 xpos=0.6 bFixed=0 pidpos=1
+OPTIONS="-bpdx 128 -bpdy 64 -tdump 0.1 -nu 0.000045 -tend 20 -poissonType cosine -iterativePensalization 0"
+OBJECTS='NACA L=0.15 xpos=0.25 bFixed=1 bForced=1 xvel=0.3 Apitch=0.15 Fpitch=2.5
+stefanfish L=0.125 xpos=0.6 bFixed=0 pidpos=1
 '
 
 source launchCommon.sh
