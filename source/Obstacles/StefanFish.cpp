453919804eb40499f0e53e9460da8b8a813f0901
Ioannis Mandralis <ioannima@student.ethz.ch>
2020-03-23 11:22:19 +0100

    brought CurvatureFish back in obstacle files

--- source/Obstacles/StefanFish.cpp
+++ source/Obstacles/StefanFish.cpp
@@ -8,12 +8,202 @@
 
 
 #include "StefanFish.h"
-#include "CurvatureFish.h"
 #include "FishLibrary.h"
 #include "FishUtilities.h"
 #include <sstream>
 
 using namespace cubism;
+
+class CurvatureFish : public FishData
+{
+ public:
+  // PID controller of body curvature:
+  Real curv_PID_fac = 0;
+  Real curv_PID_dif = 0;
+  // exponential averages:
+  Real avgDeltaY = 0;
+  Real avgDangle = 0;
+  Real avgAngVel = 0;
+  // stored past action for RL state:
+  Real lastTact = 0;
+  Real lastCurv = 0;
+  Real oldrCurv = 0;
+  // quantities needed to correctly control the speed of the midline maneuvers:
+  Real periodPIDval = Tperiod;
+  Real periodPIDdif = 0;
+  bool TperiodPID = false;
+  // quantities needed for rl:
+  Real time0 = 0;
+  Real timeshift = 0;
+  // aux quantities for PID controllers:
+  Real lastTime = 0;
+  Real lastAvel = 0;
+
+ protected:
+  Real * const rK;
+  Real * const vK;
+  Real * const rC;
+  Real * const vC;
+  Real * const rB;
+  Real * const vB;
+
+  // next scheduler is used to ramp-up the curvature from 0 during first period:
+  Schedulers::ParameterSchedulerVector<6> curvatureScheduler;
+  // next scheduler is used for midline-bending control points for RL:
+  Schedulers::ParameterSchedulerLearnWave<7> rlBendingScheduler;
+ public:
+
+  CurvatureFish(Real L, Real T, Real phi, Real _h, Real _A)
+  : FishData(L, T, phi, _h, _A),   rK(_alloc(Nm)),vK(_alloc(Nm)),
+    rC(_alloc(Nm)),vC(_alloc(Nm)), rB(_alloc(Nm)),vB(_alloc(Nm)) {
+      _computeWidth();
+      writeMidline2File(0, "initialCheck");
+    }
+
+  void resetAll() override {
+    curv_PID_fac = 0;
+    curv_PID_dif = 0;
+    avgDeltaY = 0;
+    avgDangle = 0;
+    avgAngVel = 0;
+    lastTact = 0;
+    lastCurv = 0;
+    oldrCurv = 0;
+    periodPIDval = Tperiod;
+    periodPIDdif = 0;
+    TperiodPID = false;
+    timeshift = 0;
+    lastTime = 0;
+    lastAvel = 0;
+    time0 = 0;
+    curvatureScheduler.resetAll();
+    rlBendingScheduler.resetAll();
+    FishData::resetAll();
+  }
+
+  void correctTrajectory(const Real dtheta, const Real vtheta,
+                                         const Real t, const Real dt)
+  {
+    curv_PID_fac = dtheta;
+    curv_PID_dif = vtheta;
+  }
+
+  void correctTailPeriod(const Real periodFac,const Real periodVel,
+                                        const Real t, const Real dt)
+  {
+    assert(periodFac>0 && periodFac<2); // would be crazy
+
+    const Real lastArg = (lastTime-time0)/periodPIDval + timeshift;
+    time0 = lastTime;
+    timeshift = lastArg;
+    // so that new arg is only constant (prev arg) + dt / periodPIDval
+    // with the new l_Tp:
+    periodPIDval = Tperiod * periodFac;
+    periodPIDdif = Tperiod * periodVel;
+    lastTime = t;
+    TperiodPID = true;
+  }
+
+// Execute takes as arguments the current simulation time and the time
+// the RL action should have actually started. This is important for the midline
+// bending because it relies on matching the splines with the half period of
+// the sinusoidal describing the swimming motion (in order to exactly amplify
+// or dampen the undulation). Therefore, for Tp=1, t_rlAction might be K * 0.5
+// while t_current would be K * 0.5 plus a fraction of the timestep. This
+// because the new RL discrete step is detected as soon as t_current>=t_rlAction
+  void execute(const Real t_current, const Real t_rlAction,
+                              const std::vector<double>&a)
+  {
+    assert(t_current >= t_rlAction);
+    oldrCurv = lastCurv; // store action
+    lastCurv = a[0]; // store action
+
+    rlBendingScheduler.Turn(a[0], t_rlAction);
+    printf("Turning by %g at time %g with period %g.\n",
+           a[0], t_current, t_rlAction);
+
+    if (a.size()>1) // also modify the swimming period
+    {
+      lastTact = a[1]; // store action
+      // this is arg of sinusoidal before change-of-Tp begins:
+      const Real lastArg = (t_rlAction - time0)/periodPIDval + timeshift;
+      // make sure change-of-Tp affects only body config for future times:
+      timeshift = lastArg; // add phase shift that accounts for current body shape
+      time0 = t_rlAction; // shift time axis of undulation
+      periodPIDval = Tperiod * (1 + a[1]); // actually change period
+      periodPIDdif = 0; // constant periodPIDval between infrequent actions
+    }
+  }
+
+  ~CurvatureFish() override {
+    _dealloc(rK); _dealloc(vK); _dealloc(rC); _dealloc(vC);
+    _dealloc(rB); _dealloc(vB);
+  }
+
+  void computeMidline(const Real time, const Real dt) override;
+  Real _width(const Real s, const Real L) override
+  {
+    const Real sb=.04*length, st=.95*length, wt=.01*length, wh=.04*length;
+    if(s<0 or s>L) return 0;
+    return (s<sb ? std::sqrt(2*wh*s -s*s) :
+           (s<st ? wh-(wh-wt)*std::pow((s-sb)/(st-sb),1) : // pow(.,2) is 3D
+           (wt * (L-s)/(L-st))));
+  }
+};
+
+void CurvatureFish::computeMidline(const Real t, const Real dt)
+{
+  const std::array<Real ,6> curvaturePoints = { (Real)0, (Real).15*length,
+    (Real).4*length, (Real).65*length, (Real).9*length, length
+  };
+  const std::array<Real ,6> curvatureValues = {
+      (Real)0.82014/length, (Real)1.46515/length, (Real)2.57136/length,
+      (Real)3.75425/length, (Real)5.09147/length, (Real)5.70449/length
+  };
+  const std::array<Real,7> bendPoints = {(Real)-.5, (Real)-.25, (Real)0,
+    (Real).25, (Real).5, (Real).75, (Real)1};
+
+  #if 1 // ramp-up over Tperiod
+  const std::array<Real,6> curvatureZeros = std::array<Real, 6>();
+  curvatureScheduler.transition(0,0,Tperiod,curvatureZeros ,curvatureValues);
+  #else // no rampup for debug
+  curvatureScheduler.transition(t,0,Tperiod,curvatureValues,curvatureValues);
+  #endif
+
+  curvatureScheduler.gimmeValues(t,                curvaturePoints,Nm,rS,rC,vC);
+  rlBendingScheduler.gimmeValues(t,periodPIDval,length, bendPoints,Nm,rS,rB,vB);
+
+  // next term takes into account the derivative of periodPIDval in darg:
+  const Real diffT = TperiodPID? 1 - (t-time0)*periodPIDdif/periodPIDval : 1;
+  // time derivative of arg:
+  const Real darg = 2*M_PI/periodPIDval * diffT;
+  const Real arg0 = 2*M_PI*((t-time0)/periodPIDval +timeshift) +M_PI*phaseShift;
+
+  #pragma omp parallel for schedule(static)
+  for(int i=0; i<Nm; ++i) {
+    const Real arg = arg0 - 2*M_PI*rS[i]/length/waveLength;
+    rK[i] = amplitudeFactor* rC[i]*(std::sin(arg)     + rB[i] +curv_PID_fac);
+    vK[i] = amplitudeFactor*(vC[i]*(std::sin(arg)     + rB[i] +curv_PID_fac)
+                            +rC[i]*(std::cos(arg)*darg+ vB[i] +curv_PID_dif));
+    assert(not std::isnan(rK[i]));
+    assert(not std::isinf(rK[i]));
+    assert(not std::isnan(vK[i]));
+    assert(not std::isinf(vK[i]));
+  }
+
+  // solve frenet to compute midline parameters
+  IF2D_Frenet2D::solve(Nm, rS, rK,vK, rX,rY, vX,vY, norX,norY, vNorX,vNorY);
+  #if 0
+   {
+    FILE * f = fopen("stefan_profile","w");
+    for(int i=0;i<Nm;++i)
+      fprintf(f,"%d %g %g %g %g %g %g %g %g %g\n",
+        i,rS[i],rX[i],rY[i],vX[i],vY[i],
+        vNorX[i],vNorY[i],width[i],height[i]);
+    fclose(f);
+   }
+  #endif
+}
 
 void StefanFish::resetAll() {
   CurvatureFish* const cFish = dynamic_cast<CurvatureFish*>( myFish );
