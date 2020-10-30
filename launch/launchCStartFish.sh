7b4a923ecc27a59d4fe0b282f56da5eeff630101
Michail Chatzimanolakis <mchatzim@daint105.login.cscs.ch>
2020-10-30 11:32:43 +0100

    Added script for amr+Cstart

--- launch/launchCStartFish.sh
+++ launch/launchCStartFish.sh
@@ -3,11 +3,11 @@
 # All quantities are scaled to satisfy Reynolds number 550.
 # The episode length of 1.5882352941 corresponds to [0, Tprep + 2*Tprop]
 
-BPDX=${BPDX:-32}
-BPDY=${BPDY:-32}
+BPDX=${BPDX:-8}
+BPDY=${BPDY:-8}
 CFL=${CFL:-0.1}
 
-OPTIONS="-bpdx ${BPDX} -bpdy ${BPDY} -tdump 0.01 -nu 0.0001931818182 -tend 1.5882352941 -CFL ${CFL} "
+OPTIONS="-bpdx ${BPDX} -bpdy ${BPDY} -tdump 0.01 -nu 0.0001931818182 -tend 1.5882352941 -CFL ${CFL} -Rtol 10.0 -Ctol 5.0 -levelMax 3"
 OBJECTS='cstartfish L=0.25 xpos=0.50 pid=0 bFixed=0 angle=-98
 '
 
