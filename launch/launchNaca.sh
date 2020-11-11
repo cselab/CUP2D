6daeede7d9b7ea18729c46823d24ad161b3cd626
Pascal Weber <webepasc@student.ethz.ch>
2020-11-11 14:55:21 +0100

    Greasy Files for Validation

--- launch/launchNaca.sh
+++ launch/launchNaca.sh
@@ -2,11 +2,13 @@ BPDX=${BPDX:-16}
 BPDY=${BPDY:-8}
 ANGLE=${ANGLE:-0}
 FPITCH=${FPITCH:-1}
+XPOS=${XPOS:-0.2}
 
-OPTIONS="-bpdx $BPDX -bpdy $BPDY -tdump 0.1 -nu 0.0000225 -tend 50 -poissonType cosine -iterativePensalization 1 -muteAll 0 -extent "
+# Re=1'000 <-> 0.00001125; Re=10'000 <-> 0.000001125
+OPTIONS="-bpdx $BPDX -bpdy $BPDY -tdump 0.1 -nu 0.00001125 -tend 50 -poissonType cosine -iterativePensalization 1 -muteAll 0 -extent "
 # COM OF NACA AIRFOIL IS 0.399421, ROTATION AROUND 0.1, thus fixedCenterDist=0.299412 -> CoR=0.1550882
 # Xpos=0.2 in [0,1]x[0,0.5] domain; for validation Xpos= in [0,15]x[0,7.5]
-OBJECTS='NACA L=0.15 xpos=0.2 angle=$ANGLE fixedCenterDist=0.299412 bFixed=0 xvel=0.0 Apitch=13.15 Fpitch=$FPITCH tAccel=1
+OBJECTS='NACA L=0.075 xpos=$XPOS angle=$ANGLE fixedCenterDist=0.299412 bFixed=0 xvel=0.15 Apitch=13.15 Fpitch=$FPITCH tAccel=1
 '
 
 source launchCommon.sh
