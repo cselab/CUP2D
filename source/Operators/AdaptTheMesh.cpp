3f6de60910f65550410896fa1b795a75219380a2
Michail Chatzimanolakis <michaich@eu-a6-008-18.euler.ethz.ch>
2021-03-16 15:12:20 +0100

    Refine mesh according to chi and omega at the same time, not sequentially

--- source/Operators/AdaptTheMesh.cpp
+++ source/Operators/AdaptTheMesh.cpp
@@ -10,18 +10,34 @@ void AdaptTheMesh::operator()(const double dt)
 
   sim.startProfiler("AdaptTheMesh");
 
-  //if (count % 1000 == 0) {auto K = computeDivergence(sim); K.run();}
+//  if (count % 1000 == 0) {auto K = computeDivergence(sim); K.run();}
 
   findOmega.run();//store vorticity in tmp
+  {
+    //Refine according to chi and omega. Set omega=inf wherever chi > 0.
+    const std::vector<BlockInfo>& tmpInfo = sim.tmp->getBlocksInfo();
+    const std::vector<BlockInfo>& chiInfo = sim.chi->getBlocksInfo();
+    const size_t Nblocks = tmpInfo.size();
+    #pragma omp parallel for
+    for (size_t i=0; i < Nblocks; i++)
+    {
+      auto& __restrict__ TMP = *(ScalarBlock*)  tmpInfo[i].ptrBlock;
+      auto& __restrict__ CHI = *(ScalarBlock*)  chiInfo[i].ptrBlock;
+      for(int y=0; y<VectorBlock::sizeY; ++y)
+      for(int x=0; x<VectorBlock::sizeX; ++x)
+      {
+        if (CHI(x,y).s > 0.01) TMP(x,y).s = 1e10;
+      }
+    }
+  }
 
   double Rtol = sim.Rtol;
   double Ctol = sim.Ctol;
 
   bool verbose = sim.verbose;
   ScalarAMR tmp_amr   ( *sim.tmp    ,Rtol, Ctol, verbose);//refine according to tmp (which is vorticity magnitude)
-
-  verbose = false;
   ScalarAMR chi_amr   ( *sim.chi    ,0.05, 0.01 , verbose);
+  verbose = false;
   VectorAMR vel_amr   ( *sim.vel    ,Rtol, Ctol, verbose);
   ScalarAMR pres_amr  ( *sim.pres   ,Rtol, Ctol, verbose);
   VectorAMR uDef_amr  ( *sim.uDef   ,Rtol, Ctol, verbose);  
@@ -41,16 +57,6 @@ void AdaptTheMesh::operator()(const double dt)
   tmpV_amr.AdaptLikeOther1<ScalarGrid>(*sim.tmp);
   //pOld_amr.AdaptLikeOther1<ScalarGrid>(*sim.tmp);
   pOld_amr.AdaptLikeOther <ScalarGrid>(*sim.tmp);
-
-  chi_amr .AdaptTheMesh();
-  tmp_amr .AdaptLikeOther1<ScalarGrid>(*sim.chi);
-  vel_amr .AdaptLikeOther1<ScalarGrid>(*sim.chi);
-  pres_amr.AdaptLikeOther1<ScalarGrid>(*sim.chi);
-  uDef_amr.AdaptLikeOther1<ScalarGrid>(*sim.chi);
-  dump_amr.AdaptLikeOther <ScalarGrid>(*sim.chi);
-  tmpV_amr.AdaptLikeOther1<ScalarGrid>(*sim.chi);
-  //pOld_amr.AdaptLikeOther1<ScalarGrid>(*sim.chi);
-  pOld_amr.AdaptLikeOther <ScalarGrid>(*sim.chi);
 
   sim.chi ->SortBlocks();
   sim.vel ->SortBlocks();
