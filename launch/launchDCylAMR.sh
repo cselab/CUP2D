9eff607c13e25db6b15cf4dc903adb268199251a
Michail Chatzimanolakis <michaich@eu-login-34.euler.ethz.ch>
2021-03-29 09:03:08 +0200

    Updated Cylinder launch script

--- launch/launchDCylAMR.sh
+++ launch/launchDCylAMR.sh
@@ -1,22 +1,19 @@
-# Defaults for Options
-BPDX=${BPDX:-32}
-BPDY=${BPDY:-16}
-LEVELS=${LEVELS:-3}
-LS=${LS:-1}
-RTOL=${RTOL-0.1}
+BPDX=${BPDX:-8}
+BPDY=${BPDY:-4}
+LEVELS=${LEVELS:-7}
+RTOL=${RTOL-0.50}
 CTOL=${CTOL-0.01}
-EXTENT=${EXTENT:-1}
-CFL=${CFL:-0.5}
-# Defaults for Objects
-XPOS=${XPOS:-0.2}
-ANGLE=${ANGLE:-20}
-XVEL=${XVEL:-0.15}
-RADIUS=${RADIUS:-0.0375}
-# Re=1'000 <-> 0.00001125; Re=10'000 <-> 0.000001125
-NU=${NU:-0.0001125}
+EXTENT=${EXTENT:-4.0}
+CFL=${CFL:-0.2}
+XPOS=${XPOS:-2.0}
+ANGLE=${ANGLE:-0}
+XVEL=${XVEL:-0.2}
+RADIUS=${RADIUS:-0.1}
+#NU=${NU:-0.00000421052} #Re9500
+#NU=${NU:-0.00001333333} #Re=3000
+#NU=${NU:-0.00007272727} #Re=550
+NU=${NU:-0.001} #Re=40
 
-OPTIONS="-bpdx $BPDX -bpdy $BPDY -levelMax $LEVELS -levelStart $LS -Rtol $RTOL -Ctol $CTOL -extent $EXTENT -CFL $CFL -tdump 0.1 -nu $NU -tend 10 -poissonType dirichlet -iterativePensalization 0 -muteAll 0 -verbose 1"
-# bForced, tAccel is needed here!
-OBJECTS="halfDisk radius=$RADIUS angle=$ANGLE xpos=$XPOS bForced=1 bFixed=1 xvel=$XVEL tAccel=5"
-
+OPTIONS="-bpdx $BPDX -bpdy $BPDY -levelMax $LEVELS -levelStart 3 -Rtol $RTOL -Ctol $CTOL -extent $EXTENT -CFL $CFL -tdump 1.0 -nu $NU -tend 15. -verbose 1 -lambda 1e6 "
+OBJECTS="disk radius=$RADIUS angle=$ANGLE xpos=$XPOS bForced=1 bFixed=1 xvel=$XVEL tAccel=0"
 source launchCommon.sh
