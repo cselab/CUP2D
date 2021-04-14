76d0c2aaccba75392b230d0324d5b82931125cb5
Pascal Weber <webepasc@student.ethz.ch>
2021-04-14 17:46:14 +0200

    launchDisk

--- launch/launchDiskAMR.sh
+++ launch/launchDiskAMR.sh
@@ -1,21 +1,19 @@
 # Defaults for Options
-BPDX=${BPDX:-32}
+BPDX=${BPDX:-16}
 BPDY=${BPDY:-16}
 LEVELS=${LEVELS:-3}
 RTOL=${RTOL-0.1}
 CTOL=${CTOL-0.01}
-EXTENT=${EXTENT:-1}
-CFL=${CFL:-0.5}
+EXTENT=${EXTENT:-4}
+CFL=${CFL:-0.2}
 # Defaults for Objects
-XPOS=${XPOS:-0.2}
-ANGLE=${ANGLE:-0}
-XVEL=${XVEL:-0.15}
-RADIUS=${RADIUS:-0.0375}
-# Re=100 <-> 0.0001125; Re=1'000 <-> 0.00001125; Re=2'500 <-> 0.0000045; Re=5'000 <-> 0.00000225; Re=7'500 <-> 0.0000015; Re=10'000 <-> 0.000001125
-NU=${NU:-0.0001125}
+XPOS=${XPOS:-1.2}
+XVEL=${XVEL:-0.2}
+RADIUS=${RADIUS:-0.1}
+# Re=40 <-> NU=0.001; Re=550 <-> NU=0.00007272727273; Re=3000 <-> NU=0.00001333333333; Re=9'500 <-> NU=0.000004210526316; Re=40'000 <-> 0.000001; Re=100'000 <-> NU=0.0000004
+NU=${NU:-0.0000004}
 
-OPTIONS="-bpdx $BPDX -bpdy $BPDY -levelMax $LEVELS -Rtol $RTOL -Ctol $CTOL -extent $EXTENT -CFL $CFL -tdump 0.1 -nu $NU -tend 10 -poissonType dirichlet -iterativePensalization 0 -muteAll 0 -verbose 1"
-# bForced, tAccel is needed here!
-OBJECTS="disk radius=$RADIUS angle=$ANGLE xpos=$XPOS bForced=1 bFixed=1 xvel=$XVEL tAccel=0"
+OPTIONS="-bpdx $BPDX -bpdy $BPDY -levelMax $LEVELS -levelStart 3 -Rtol $RTOL -Ctol $CTOL -extent $EXTENT -CFL $CFL -tdump 0.1 -nu $NU -tend 100 -muteAll 0 -verbose 0"
+OBJECTS="disk radius=$RADIUS xpos=$XPOS bForced=1 bFixed=1 xvel=$XVEL tAccel=0"
 
 source launchCommon.sh
