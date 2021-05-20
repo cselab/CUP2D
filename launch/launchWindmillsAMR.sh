98620a5c62f7c54658674f5991158b36c63cb3a3
Aurelio Noca <anoca@daint103.login.cscs.ch>
2021-05-20 09:00:29 +0200

    launch

--- launch/launchWindmillsAMR.sh
+++ launch/launchWindmillsAMR.sh
@@ -1,17 +1,23 @@
 # Defaults for Options
 BPDX=${BPDX:-8}
 BPDY=${BPDY:-8}
-LEVELS=${LEVELS:-4}
+LEVELS=${LEVELS:-6}
 RTOL=${RTOL:-2}
 CTOL=${CTOL:-0.2}
-EXTENT=${EXTENT:-1}
+EXTENT=${EXTENT:-1.2}
 CFL=${CFL:-0.5}
 # Defaults for Objects
 XPOS=${XPOS:-0.2}
-YPOS1=${YPOS:-0.2}
-YPOS2=${YPOS2:-0.4}
-YPOS3=${YPOS3:-0.6}
-YPOS4=${YPOS4:-0.8}
+
+# YPOS1=${YPOS:-0.2}
+# YPOS2=${YPOS2:-0.4}
+# YPOS3=${YPOS3:-0.6}
+# YPOS4=${YPOS4:-0.8}
+
+YPOS1=${YPOS:-0.3}
+YPOS2=${YPOS2:-0.5}
+YPOS3=${YPOS3:-0.7}
+YPOS4=${YPOS4:-0.9}
 
 XVEL=${XVEL:-0.15}
 
@@ -19,9 +25,9 @@ MAAXIS=${MAAXIS:-0.0405}
 MIAXIS=${MIAXIS:-0.0135}
 
 #NU=${NU:-0.0001215}
-#NU=${NU:-0.0000243}
+NU=${NU:-0.0000243}
 #NU=${NU:-0.00001215}
-NU=${NU:-0.00000305}
+#NU=${NU:-0.00000305}
 
 
 OPTIONS="-bpdx $BPDX -bpdy $BPDY -levelMax $LEVELS -Rtol $RTOL -Ctol $CTOL -extent $EXTENT -CFL $CFL -tdump 0.1 -nu $NU -tend 20 -muteAll 0 -verbose 1"
