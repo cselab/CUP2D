af4bcbc8e758b072d65a5d3706b1c6e2e99a7a8d
Stephanie Mueller <mstephan@daint106.login.cscs.ch>
2021-10-18 15:28:39 +0200

    launch with s929 allocation

--- launch/launchSbatch.sh
+++ launch/launchSbatch.sh
@@ -8,14 +8,14 @@ cd ${FOLDERNAME}
 cat <<EOF >daint_sbatch
 #!/bin/bash -l
 
-#SBATCH --account=eth2
+#SBATCH --account=s929
 #SBATCH --job-name="${RUNNAME}"
 #SBATCH --output=${RUNNAME}_out_%j.txt
 #SBATCH --error=${RUNNAME}_err_%j.txt
 #SBATCH --time=24:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --constraint=mc
+#SBATCH --constraint=gpu
 
 # #SBATCH --account=s929
 # #SBATCH --job-name="${RUNNAME}"
@@ -33,12 +33,12 @@ cat <<EOF >daint_sbatch
 # #SBATCH --mail-user="${MYNAME}@ethz.ch"
 # #SBATCH --mail-type=ALL
 
-export OMP_NUM_THREADS=36
+export OMP_NUM_THREADS=12
 #export CRAY_CUDA_MPS=1
 #export OMP_PROC_BIND=CLOSE
 #export OMP_PLACES=cores
 
-srun --ntasks 1 --cpus-per-task=36 --threads-per-core=1 --ntasks-per-node=1 ./simulation ${OPTIONS} -shapes "${OBJECTS}"
+srun --ntasks 1 --cpus-per-task=12 --threads-per-core=1 --ntasks-per-node=1 ./simulation ${OPTIONS} -shapes "${OBJECTS}"
 EOF
 
 chmod 755 daint_sbatch
