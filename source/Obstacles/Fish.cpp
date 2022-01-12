0d0b43d518bb3089a72036cfb02d417558cf2822
Michail Chatzimanolakis <mchatzim@daint106.login.cscs.ch>
2022-01-12 13:59:04 +0100

    restarts for obstacles that rotate (angle added)

--- source/Obstacles/Fish.cpp
+++ source/Obstacles/Fish.cpp
@@ -203,3 +203,22 @@ void Fish::removeMoments(const std::vector<cubism::BlockInfo>& vInfo)
   }
   #endif
 }
+
+void Fish::saveRestart( FILE * f ) {
+  assert(f != NULL);
+  Shape::saveRestart(f);
+  fprintf(f, "theta_internal: %20.20e\n", theta_internal );
+  fprintf(f, "angvel_internal: %20.20e\n", angvel_internal );
+}
+
+void Fish::loadRestart( FILE * f ) {
+  assert(f != NULL);
+  Shape::loadRestart(f);
+  bool ret = true;
+  ret = ret && 1==fscanf(f, "theta_internal: %le\n", &theta_internal );
+  ret = ret && 1==fscanf(f, "angvel_internal: %le\n", &angvel_internal );
+  if( (not ret) ) {
+    printf("Error reading restart file. Aborting...\n");
+    fflush(0); abort();
+  }
+}
