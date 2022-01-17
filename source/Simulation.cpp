fd7e819c42a3533458c0b69293ce6852e98294a7
Ivica Kicic <kicici@ethz.ch>
2022-01-17 16:56:11 +0100

    less output when muteAll is set

--- source/Simulation.cpp
+++ source/Simulation.cpp
@@ -335,7 +335,7 @@ void Simulation::startObstacles()
 }
 
 void Simulation::simulate() {
-  if (sim.rank == 0)
+  if (sim.rank == 0 && !sim.muteAll)
     std::cout << kHorLine << "[CUP2D] Starting Simulation...\n" << std::flush;
 
   while (1)
@@ -353,7 +353,7 @@ void Simulation::simulate() {
 
     if (done)
     {
-      if (sim.rank == 0)
+      if (sim.rank == 0 && !sim.muteAll)
       {
         std::cout << kHorLine << "[CUP2D] Simulation Over... Profiling information:\n";
         sim.printResetProfiler();
@@ -410,7 +410,7 @@ void Simulation::advance(const Real dt)
 {
 
   const Real CFL = ( sim.uMax_measured + 1e-8 ) * sim.dt / sim.getH();
-  if (sim.rank == 0)
+  if (sim.rank == 0 && !sim.muteAll)
   {
     std::cout << kHorLine;
     printf("[CUP2D] step:%d, time:%f, dt=%f, uinf:[%f %f], maxU:%f, CFL:%f\n",
