b9e571f249453477ca403bf6957be7d6b9762e6a
Michail Chatzimanolakis <mchatzim@daint103.login.cscs.ch>
2022-01-19 11:16:23 +0100

    Improved viscous force computation (part 2)

--- source/Operators/ComputeForces.cpp
+++ source/Operators/ComputeForces.cpp
@@ -80,7 +80,7 @@ struct KernelComputeForces
           for (int kk = 1 ; kk < 10 ; kk++) //10 is arbitrary
           {
             if ((int)abs(kk*dx_a) > 3 || (int)abs(kk*dy_a) > 3) break; //3 means we moved too far
-            if (chi(x,y).s <3e-1 && found >= 2) break;
+            if (chi(x,y).s <3e-1 && found >= 1) break;
             x  = ix + kk*dx_a; 
             y  = iy + kk*dy_a;
             if (chi(x,y).s < 1e-3 ) found ++;
