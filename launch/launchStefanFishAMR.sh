a864223384ae1a8521df739495e31bca23475833
Michail Chatzimanolakis <mchatzim@daint101.login.cscs.ch>
2022-04-08 13:51:20 +0200

    minor modification is launch/launchStefanFishAMR.sh

--- launch/launchStefanFishAMR.sh
+++ launch/launchStefanFishAMR.sh
@@ -4,7 +4,7 @@
 BPDX=${BPDX:-4}
 BPDY=${BPDY:-2}
 LEVELS=${LEVELS:-7}
-RTOL=${RTOL-5}
+RTOL=${RTOL-1.0}
 CTOL=${CTOL-0.01}
 EXTENT=${EXTENT:-2}
 CFL=${CFL:-0.4}
@@ -25,7 +25,7 @@ PERIOD=${PERIOD:-1}
 ####################################
 NU=${NU:-0.00004}
 
-OPTIONS="-bpdx $BPDX -bpdy $BPDY -levelMax $LEVELS -levelStart 4  -Rtol $RTOL -Ctol $CTOL -extent $EXTENT -CFL $CFL -poissonTol $PT -poissonTolRel $PTR -bMeanConstraint 1 -bAdaptChiGradient 0 -tdump 0.1 -nu $NU -tend 100 -muteAll 0 -verbose 0"
+OPTIONS="-bpdx $BPDX -bpdy $BPDY -levelMax $LEVELS -levelStart 4  -Rtol $RTOL -Ctol $CTOL -extent $EXTENT -CFL $CFL -poissonTol $PT -poissonTolRel $PTR -bMeanConstraint 1 -bAdaptChiGradient 0 -tdump 0.1 -nu $NU -tend 5.0 -muteAll 0 -verbose 1"
 OBJECTS="stefanfish L=$LENGTH T=$PERIOD xpos=$XPOS bFixed=1
 "
 
