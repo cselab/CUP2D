e1f7b651a846f701e249e3549ab81782b52a46bb
michaich <michaich@ethz.ch>
2022-12-10 22:22:45 +0200

    optimizations in Poisson solver to overlap reductions with computation

--- source/Poisson/AMRSolver.cpp
+++ source/Poisson/AMRSolver.cpp
@@ -10,68 +10,60 @@ using namespace cubism;
 
 void AMRSolver::getZ(BlockInfo & zInfo)
 {
-  const int BSX = VectorBlock::sizeX;
-  const int BSY = VectorBlock::sizeY;
-  const int N   = BSX*BSY;
+  const int BSX  = VectorBlock::sizeX;
+  const int BSY  = VectorBlock::sizeY;
+  const int N    = BSX*BSY;
+  const int bpdx = sim.chi->getMaxBlocks()[0];
+  const int bpdy = sim.chi->getMaxBlocks()[1];
+  const int tid  = omp_get_thread_num();
 
+  ScalarBlock & __restrict__ z  = *(ScalarBlock*) zInfo.ptrBlock;
+
+  //This is done to preserve symmetries. All it does is change the order with
+  //which the elements of z(x,y) are accessed (x=0,...,N-1 or x=N-1,...0, same for y)
+  const Real c11 = z(BSX - 1, BSY - 1).s;
+  const Real c10 = z(BSX - 1,       0).s;
+  const Real c01 = z(0      , BSY - 1).s;
+  const Real c00 = z(0      ,       0).s;
+  bool plus_x = zInfo.index[0] > bpdx * (1 << zInfo.level) / 2 - 1;
+  bool plus_y = zInfo.index[1] > bpdy * (1 << zInfo.level) / 2 - 1;
+  if ( std::fabs(std::fabs(c11 + c10) - std::fabs(c01 + c00)) > 1e-14) plus_x = std::fabs(c11 + c10) > std::fabs(c01 + c00);
+  if ( std::fabs(std::fabs(c11 + c01) - std::fabs(c10 + c00)) > 1e-14) plus_y = std::fabs(c11 + c01) > std::fabs(c10 + c00);
+  const int base_x = plus_x ? 0 : BSX - 1;
+  const int base_y = plus_y ? 0 : BSY - 1;
+  const int sign_x = plus_x ? 1 : -1;
+  const int sign_y = plus_y ? 1 : -1;
+
+  //1. z = L^{-1}r
+  for (int I = 0; I < N ; I++)
   {
-    const int bpdx = sim.chi->getMaxBlocks()[0];
-    const int bpdy = sim.chi->getMaxBlocks()[1];
-    const int tid = omp_get_thread_num();
-    {
-      ScalarBlock & __restrict__ z  = *(ScalarBlock*) zInfo.ptrBlock;
-
-      //This is done to preserve symmetries. All it does is change the order with
-      //which the elements of z(x,y) are accessed (x=0,...,N-1 or x=N-1,...0, same for y)
-      const Real c11 = z(BSX - 1, BSY - 1).s;
-      const Real c10 = z(BSX - 1,       0).s;
-      const Real c01 = z(0      , BSY - 1).s;
-      const Real c00 = z(0      ,       0).s;
-      bool plus_x = zInfo.index[0] > bpdx * (1 << zInfo.level) / 2 - 1;
-      bool plus_y = zInfo.index[1] > bpdy * (1 << zInfo.level) / 2 - 1;
-      if ( std::fabs(std::fabs(c11 + c10) - std::fabs(c01 + c00)) > 1e-14) plus_x = std::fabs(c11 + c10) > std::fabs(c01 + c00);
-      if ( std::fabs(std::fabs(c11 + c01) - std::fabs(c10 + c00)) > 1e-14) plus_y = std::fabs(c11 + c01) > std::fabs(c10 + c00);
-      const int base_x = plus_x ? 0 : BSX - 1;
-      const int base_y = plus_y ? 0 : BSY - 1;
-      const int sign_x = plus_x ? 1 : -1;
-      const int sign_y = plus_y ? 1 : -1;
-
-      //1. z = L^{-1}r
-      for (int I = 0; I < N ; I++)
+      Real rhs = 0.0;
+      for (size_t jj = 0 ; jj < L_row[tid][I].size(); jj++)
       {
-        Real rhs = 0.0;
-        for (size_t jj = 0 ; jj < L_row[tid][I].size(); jj++)
-        {
           const int J = L_row[tid][I][jj].first;
           const int iy = base_y + sign_y * J / BSX;
           const int ix = base_x + sign_x * J % BSX;
           rhs += L_row[tid][I][jj].second*z(ix,iy).s;
-        }
-        const int iy = base_y + sign_y * I / BSX;
-        const int ix = base_x + sign_x * I % BSX;
-        z(ix,iy).s = (z(ix,iy).s - rhs)*Ld[tid][I];
       }
+      const int iy = base_y + sign_y * I / BSX;
+      const int ix = base_x + sign_x * I % BSX;
+      z(ix,iy).s = (z(ix,iy).s - rhs)*Ld[tid][I];
+  }
 
-      //2. z = L^T{-1}r
-      for (int I = N-1; I >= 0 ; I--)
+  //2. z = L^T{-1}r
+  for (int I = N-1; I >= 0 ; I--)
+  {
+      Real rhs = 0.0;
+      for (size_t jj = 0 ; jj < L_col[tid][I].size(); jj++)
       {
-        Real rhs = 0.0;
-        for (size_t jj = 0 ; jj < L_col[tid][I].size(); jj++)
-        {
           const int J = L_col[tid][I][jj].first;
           const int iy = base_y + sign_y * J / BSX;
           const int ix = base_x + sign_x * J % BSX;
-          rhs += L_col[tid][I][jj].second*z(ix,iy).s;
-        }
-        const int iy = base_y + sign_y * I / BSX;
-        const int ix = base_x + sign_x * I % BSX;
-        z(ix,iy).s = (z(ix,iy).s - rhs) *Ld[tid][I];
+          rhs -= L_col[tid][I][jj].second*z(ix,iy).s;
       }
-
-      for (int iy=0;iy<BSY;iy++)
-        for (int ix=0;ix<BSX;ix++)
-          z(ix,iy).s = -z(ix,iy).s;
-    }
+      const int iy = base_y + sign_y * I / BSX;
+      const int ix = base_x + sign_x * I % BSX;
+      z(ix,iy).s = (rhs - z(ix,iy).s) *Ld[tid][I];
   }
 }
 
@@ -96,7 +88,7 @@ Real AMRSolver::getA_local(const int I1,const int I2)
   }
 }
 
-AMRSolver::AMRSolver(SimulationData& s):sim(s),Get_LHS(s)
+AMRSolver::AMRSolver(SimulationData& ss):sim(ss),Get_LHS(ss)
 {
   const int BSX = VectorBlock::sizeX;
   const int BSY = VectorBlock::sizeY;
@@ -152,36 +144,46 @@ void AMRSolver::solve(const ScalarGrid *input, ScalarGrid * const output)
   if (input != sim.tmp || output != sim.pres)
     throw std::invalid_argument("AMRSolver hardcoded to sim.tmp and sim.pres for now");
 
-  const int BSX = VectorBlock::sizeX;
-  const int BSY = VectorBlock::sizeY;
-  const MPI_Comm m_comm = sim.chi->getWorldComm();
-  const int rank = sim.rank;
-
-  // The bi-conjugate gradient method needs the following 8 arrays:
-  // r: residual vector
-  // rhat: (2nd residual vector)
-  // p: conjugate vector
-  // v: bi-conjugate vector
-  // s vector
-  // Ax vector (stores the LHS computation)
-  // x vector (current solution estimate)
-  // z vector (used for preconditioning)
-
-  std::vector<cubism::BlockInfo>& AxInfo = sim.tmp ->getBlocksInfo();
-  std::vector<cubism::BlockInfo>&  zInfo = sim.pres->getBlocksInfo();
-  const size_t Nblocks = zInfo.size();
-  const size_t N = BSX*BSY*Nblocks;
-  std::vector<Real> x   (N);
-  std::vector<Real> r   (N);
-  std::vector<Real> p   (N,0.0); //initialize p = 0
-  std::vector<Real> v   (N,0.0); //initialize v = 0
-  std::vector<Real> s   (N);
-  std::vector<Real> rhat(N);
-  std::vector <Real> x_opt(N);
-
-
-  //Warning: AxInfo (sim.tmp) initially contains the RHS of the system!
+  //MPI stuff & grid size parameters
+  //Warning: AxInfo (sim.tmp ) initially contains the RHS of the system!
   //Warning:  zInfo (sim.pres) initially contains the initial solution guess x0!
+  auto & AxInfo         = sim.tmp ->getBlocksInfo(); //will store the LHS result
+  auto &  zInfo         = sim.pres->getBlocksInfo(); //used for preconditioning
+  const size_t Nblocks  = zInfo.size();
+  const int BSX         = VectorBlock::sizeX;
+  const int BSY         = VectorBlock::sizeY;
+  const size_t N        = BSX*BSY*Nblocks;
+  const MPI_Comm m_comm = sim.chi->getWorldComm();
+  const int rank        = sim.rank;
+
+  //Set some bi-conjugate gradient parameters
+  Real rho                 = 1.0;
+  Real alpha               = 1.0;
+  Real omega               = 1.0;
+  const Real eps           = 1e-100;
+  const Real max_error     = sim.step < 10 ? 0.0 : sim.PoissonTol;
+  const Real max_rel_error = sim.step < 10 ? 0.0 : sim.PoissonTolRel;
+  Real min_norm            = 1e50;
+  bool useXopt             = false;
+  bool serious_breakdown   = false;
+  int restarts             = 0;
+  const int max_restarts   = sim.step < 10 ? 100 : sim.maxPoissonRestarts;
+  bool bConverged          = false;
+  Real norm                = 0.0;     //residual norm
+  Real norm_opt            = 0.0; //residual norm (for best x)
+  Real norm_1              = 0.0;
+  Real norm_2              = 0.0;
+  Real rho_m1              = rho;
+  rho = 0.0;
+
+  // The bi-conjugate gradient arrays:
+  x    .resize(N);//     solution vector
+  r    .resize(N);//     residual vector
+  p    .resize(N);//    conjugate vector (need to set this to zero!)
+  v    .resize(N);// bi-conjugate vector (need to set this to zero!)
+  s    .resize(N);//            s-vector
+  rhat .resize(N);// 2nd residual vector
+  x_opt.resize(N);// solution with minimum residual
 
   //1. r = RHS - Ax_0, x_0: initial solution guess
   //   - (1a) We set r = RHS and store x0 in x
@@ -195,6 +197,8 @@ void AMRSolver::solve(const ScalarGrid *input, ScalarGrid * const output)
     for(int iy=0; iy<BSY; iy++)
     for(int ix=0; ix<BSX; ix++)
     {
+      p[i*BSX*BSY+iy*BSX+ix] = 0.0;
+      v[i*BSX*BSY+iy*BSX+ix] = 0.0;
       r[i*BSX*BSY+iy*BSX+ix] = rhs(ix,iy).s;
       x[i*BSX*BSY+iy*BSX+ix] = z  (ix,iy).s;
     }
@@ -204,118 +208,36 @@ void AMRSolver::solve(const ScalarGrid *input, ScalarGrid * const output)
 
   //   - (1c) We substract A*x0 from r. 
   //          Here we also set rhat = r and compute the initial guess error norm.
-  Real norm = 0.0; //initial residual norm
-  Real norm_opt = 0.0; //initial residual norm
-  #pragma omp parallel for reduction (max:norm)
+  #pragma omp parallel for reduction(+:rho,norm_1,norm_2)reduction(max:norm)
   for (size_t i=0; i < Nblocks; i++)
   {
     const ScalarBlock & __restrict__ lhs  = *(ScalarBlock*)  AxInfo[i].ptrBlock;
-    Real norm_max_local = 0.0;
     for(int iy=0; iy<BSY; iy++)
     for(int ix=0; ix<BSX; ix++)
     {
-      r[i*BSX*BSY+iy*BSX+ix] -= lhs(ix,iy).s;
-      rhat[i*BSX*BSY+iy*BSX+ix]  = r[i*BSX*BSY+iy*BSX+ix];
-      norm_max_local = std::max(norm_max_local,std::fabs(r[i*BSX*BSY+iy*BSX+ix]));
+      const int idx = i*BSX*BSY+iy*BSX+ix;
+      r[idx] -= lhs(ix,iy).s;
+      rhat[idx]  = r[idx];
+      norm = std::max(norm,std::fabs(r[idx]));
+      rho    += r[idx] * rhat[idx];
+      norm_1 += r[idx] * r[idx];
+      norm_2 += rhat[idx] * rhat[idx];
     }
-    norm = std::max(norm_max_local,norm);
   }
   MPI_Allreduce(MPI_IN_PLACE,&norm,1,MPI_Real,MPI_MAX,m_comm);
-
-  //2. Set some bi-conjugate gradient parameters
-  Real rho   = 1.0;
-  Real alpha = 1.0;
-  Real omega = 1.0;
-  const Real eps = 1e-21;
-  const Real max_error     = sim.step < 10 ? 0.0 : sim.PoissonTol;//* sim.uMax_measured / sim.dt;
-  const Real max_rel_error = sim.step < 10 ? 0.0 : sim.PoissonTolRel;//min(1e-2,sim.PoissonTolRel * sim.uMax_measured / sim.dt );
-  Real min_norm = 1e50;
-  Real rho_m1;
-  Real init_norm=norm;
-  bool useXopt = false;
-  bool serious_breakdown = false;
-  int restarts = 0;
-  const int max_restarts = sim.step < 10 ? 100 : sim.maxPoissonRestarts;
-  bool bConverged = false;
-
-  //3. start iterations
-  Real norm_1 = 0.0;
-  Real norm_2 = 0.0;
-  int k;
+  const Real init_norm=norm;
   if (rank == 0 && !sim.muteAll)
     std::cout << "  [Poisson solver]: Initial norm: " << init_norm << std::endl;
+
+
+  MPI_Request request_norm;
+  Real beta = 0.0;
+
+  //3. start iterations
+  int k;
   const int kmax = sim.maxPoissonIterations;
   for ( k = 0 ; k < kmax; k++)
   {
-    //1. rho_{k} = rhat_0 * rho_{k-1}
-    //2. beta = rho_{k} / rho_{k-1} * alpha/omega 
-
-    if (k==0)
-    {
-      rho_m1 = rho;
-      rho = 0.0;
-      norm = 0;
-      norm_1 = 0;
-      norm_2 = 0;
-      #pragma omp parallel for reduction(+:rho,norm_1,norm_2)reduction(max:norm)
-      for(size_t i=0; i< N; i++)
-      {
-        rho    += r[i] * rhat[i];
-        norm_1 += r[i] * r[i];
-        norm_2 += rhat[i] * rhat[i];
-        norm = std::max(norm,std::fabs(r[i]));
-      }
-      MPI_Allreduce(MPI_IN_PLACE,&norm,1,MPI_Real,MPI_MAX,m_comm);
-    }
-    if (norm < min_norm)
-    {
-      norm_opt = norm;
-      useXopt = true;
-      min_norm = norm;
-      #pragma omp parallel for
-      for (size_t i=0; i < N; i++)
-        x_opt[i] = x[i];
-    }
-    if ( (norm < max_error || norm/init_norm < max_rel_error ) )
-    {
-      if (rank == 0 && !sim.muteAll)
-        std::cout << "  [Poisson solver]: Converged after " << k << " iterations.\n";
-      bConverged = true;
-      break;
-    }
-    Real quantities[3] = {rho,norm_1,norm_2};
-    MPI_Allreduce(MPI_IN_PLACE,&quantities,3,MPI_Real,MPI_SUM,m_comm);
-    rho = quantities[0]; norm_1 = quantities[1] ; norm_2 = quantities[2];
-
-    Real beta = rho / (rho_m1+eps) * alpha / (omega+eps);
-
-    //Check if restart should be made. If so, current solution estimate is used as an initial
-    //guess and solver starts again.
-    // const Real cosTheta = rho/sqrt(norm_1*norm_2);
-    // serious_breakdown = std::fabs(cosTheta) < 1e-8;
-    serious_breakdown = rho * rho < 1e-16 * norm_1 * norm_2;
-    if (serious_breakdown && restarts < max_restarts)
-    {
-      restarts ++;
-      if (rank == 0 && !sim.muteAll)
-        std::cout << "  [Poisson solver]: Restart at iteration: " << k << " norm: " << norm <<" Initial norm: " << init_norm << std::endl;
-      beta = 0.0;
-      rho = 0.0;
-      #pragma omp parallel for reduction(+:rho)
-      for(size_t i=0; i< N; i++)
-      {
-        rhat[i] = r[i];
-        rho += r[i]*rhat[i];
-        p[i] = 0;
-        v[i] = 0;
-      }
-      MPI_Allreduce(MPI_IN_PLACE,&rho,1,MPI_Real,MPI_SUM,m_comm);
-      alpha = 1.;
-      omega = 1.;
-      rho_m1 = 1.;
-      beta = rho / (rho_m1+eps) * alpha / (omega+eps) ;
-    }
-
     //3. p_{k} = r_{k-1} + beta*(p_{k-1}-omega *v_{k-1})
     //4. z = K_2 ^{-1} p
     #pragma omp parallel for
@@ -360,6 +282,31 @@ void AMRSolver::solve(const ScalarGrid *input, ScalarGrid * const output)
     }
     MPI_Allreduce(MPI_IN_PLACE,&alpha,1,MPI_Real,MPI_SUM,m_comm);
     alpha = rho / (alpha + eps);
+
+    if ( k > 0 ) MPI_Waitall(1,&request_norm,MPI_STATUSES_IGNORE);
+    if (norm / (init_norm + 1e-21) > 1e10)
+    {
+      if (rank == 0 && !sim.muteAll)
+        std::cout << "   [Poisson solver]: early termination." << std::endl;
+      break;
+    }
+    if (norm < min_norm)
+    {
+      norm_opt = norm;
+      useXopt = true;
+      min_norm = norm;
+      #pragma omp parallel for
+      for (size_t i=0; i < N; i++)
+        x_opt[i] = x[i];
+    }
+    if ( (norm < max_error || norm/init_norm < max_rel_error ) )
+    {
+      if (rank == 0 && !sim.muteAll)
+        std::cout << "  [Poisson solver]: Converged after " << k << " iterations.\n";
+      bConverged = true;
+      break;
+    }
+
     #pragma omp parallel for
     for (size_t i=0; i < Nblocks; i++)
     {
@@ -413,25 +360,51 @@ void AMRSolver::solve(const ScalarGrid *input, ScalarGrid * const output)
       for(int ix=0; ix<BSX; ix++)
       {
         const int j = i*BSX*BSY+iy*BSX+ix;
-        x[j] += omega * z(ix,iy).s;
-        r[j] = s[j] - omega * Ax(ix,iy).s;
-
+        x[j]   += omega * z(ix,iy).s;
+        r[j]    = s[j] - omega * Ax(ix,iy).s;
         rho    += r[j] * rhat[j];
         norm_1 += r[j] * r[j];
         norm_2 += rhat[j] * rhat[j];
         norm = std::max(norm,std::fabs(r[j]));
       }
     }
-    MPI_Allreduce(MPI_IN_PLACE,&norm,1,MPI_Real,MPI_MAX,m_comm);
+    MPI_Iallreduce(MPI_IN_PLACE,&norm,1,MPI_Real,MPI_MAX,m_comm,&request_norm);
 
-    if (norm / (init_norm + 1e-21) > 1e10)
+    Real quantities[3] = {rho,norm_1,norm_2};
+    MPI_Allreduce(MPI_IN_PLACE,&quantities,3,MPI_Real,MPI_SUM,m_comm);
+    rho = quantities[0]; norm_1 = quantities[1] ; norm_2 = quantities[2];
+
+    beta = rho / (rho_m1+eps) * alpha / (omega+eps);
+
+    //Check if restart should be made. If so, current solution estimate is used as an initial
+    //guess and solver starts again.
+    serious_breakdown = rho * rho < 1e-16 * norm_1 * norm_2;
+    if (serious_breakdown && restarts < max_restarts)
     {
+      if ( k > 0 ) MPI_Waitall(1,&request_norm,MPI_STATUSES_IGNORE);
+
+      restarts ++;
       if (rank == 0 && !sim.muteAll)
-        std::cout << "   [Poisson solver]: early termination." << std::endl;
-      break;
+        std::cout << "  [Poisson solver]: Restart at iteration: " << k << " norm: " << norm <<" Initial norm: " << init_norm << std::endl;
+      beta = 0.0;
+      rho = 0.0;
+      #pragma omp parallel for reduction(+:rho)
+      for(size_t i=0; i< N; i++)
+      {
+        rhat[i] = r[i];
+        rho += r[i]*rhat[i];
+        p[i] = 0;
+        v[i] = 0;
+      }
+      MPI_Allreduce(MPI_IN_PLACE,&rho,1,MPI_Real,MPI_SUM,m_comm);
+      alpha = 1.;
+      omega = 1.;
+      rho_m1 = 1.;
+      beta = rho / (rho_m1+eps) * alpha / (omega+eps) ;
     }
 
   } //k-loop
+
   if (rank == 0 && !sim.muteAll)
   {
     if( bConverged )
@@ -474,14 +447,4 @@ void AMRSolver::solve(const ScalarGrid *input, ScalarGrid * const output)
     for(int ix=0; ix<BSX; ix++)
        P(ix,iy).s -= avg;
   }
-
-  #if 0
-  if (rank == 0)
-  {
-    ofstream myfile;
-    myfile.open ("iterations.txt",ios::app);
-    myfile << sim.step << " " << k << " " << norm << std::endl;
-    myfile.close();
-  }
-  #endif
 }
--- source/Poisson/AMRSolver.h
+++ source/Poisson/AMRSolver.h
@@ -15,7 +15,7 @@ class ComputeLHS : public Operator
 {
   struct LHSkernel
   {
-    LHSkernel(const SimulationData & s) : sim(s) {}
+    LHSkernel(const SimulationData & ss) : sim(ss) {}
     const SimulationData & sim;
     const cubism::StencilInfo stencil{-1, -1, 0, 2, 2, 1, false, {0}};
     const std::vector<cubism::BlockInfo>& lhsInfo = sim.tmp->getBlocksInfo();
@@ -67,12 +67,11 @@ class ComputeLHS : public Operator
     }
   };
   public:
-  ComputeLHS(SimulationData & s) : Operator(s) { }
+  ComputeLHS(SimulationData & ss) : Operator(ss) { }
   bool isCorner(cubism::BlockInfo & info)
   {
-    //const int aux = 1 << info.level;
-    const bool x = info.index[0] == 0;//(sim.bpdx * aux - 1)/ 2;
-    const bool y = info.index[1] == 0;//(sim.bpdy * aux - 1)/ 2;
+    const bool x = info.index[0] == 0;
+    const bool y = info.index[1] == 0;
     return x && y;
   }
 
@@ -81,7 +80,8 @@ class ComputeLHS : public Operator
   {
     const LHSkernel K(sim);
     cubism::compute<ScalarLab>(K,sim.pres,sim.tmp);
-    if( sim.bMeanConstraint ) {
+    if( sim.bMeanConstraint )
+    {
       int index = -1;
       Real mean = 0.0;
       std::vector<cubism::BlockInfo>& lhsInfo = sim.tmp->getBlocksInfo();
@@ -90,7 +90,7 @@ class ComputeLHS : public Operator
       for (size_t i = 0 ; i < lhsInfo.size() ; i++)
       {
        cubism::BlockInfo & info = lhsInfo[i];
-       if ( isCorner(info) ) index = i;//info.blockID;
+       if ( isCorner(info) ) index = i;
        const Real h2 = info.h*info.h;
        ScalarBlock & __restrict__ X   = *(ScalarBlock*) xInfo[info.blockID].ptrBlock;
        for(int iy=0; iy<ScalarBlock::sizeY; ++iy)
@@ -103,6 +103,16 @@ class ComputeLHS : public Operator
        ScalarBlock & __restrict__ LHS = *(ScalarBlock*) lhsInfo[index].ptrBlock;
        LHS(0,0).s = mean;
       }
+      /*
+      for (size_t i = 0 ; i < lhsInfo.size() ; i++)
+      {
+         ScalarBlock & __restrict__ LHS = *(ScalarBlock*) lhsInfo[i].ptrBlock;
+         const Real h2 = lhsInfo[i].h*lhsInfo[i].h;
+         for(int iy=0; iy<ScalarBlock::sizeY; ++iy)
+         for(int ix=0; ix<ScalarBlock::sizeX; ++ix)
+             LHS(ix,iy).s += mean * h2;
+      }
+      */
     }
   }
   std::string getName() { return "ComputeLHS"; }
@@ -116,7 +126,7 @@ class AMRSolver : public PoissonSolver
   std::string getName() {
     return "AMRSolver";
   }
-  AMRSolver(SimulationData& s);
+  AMRSolver(SimulationData& ss);
   void solve(const ScalarGrid *input, ScalarGrid *output) override;
   ComputeLHS Get_LHS;
   std::vector<std::vector<Real>> Ld;
@@ -124,6 +134,14 @@ class AMRSolver : public PoissonSolver
   std::vector <  std::vector <std::vector< std::pair<int,Real> > > >L_col;
   void getZ(cubism::BlockInfo & zInfo);
   Real getA_local(const int I1, const int I2);
+
+  std::vector<Real> x   ;
+  std::vector<Real> r   ;
+  std::vector<Real> p   ; //initialize p = 0
+  std::vector<Real> v   ; //initialize v = 0
+  std::vector<Real> s   ;
+  std::vector<Real> rhat;
+  std::vector<Real> x_opt;
 
   bool isCorner(cubism::BlockInfo & info)
   {
