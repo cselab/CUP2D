f68c140e879ace609842d44bc61f4e98bc955117
Michail Chatzimanolakis <mchatzim@daint106.login.cscs.ch>
2023-02-13 16:56:25 +0100

    Smooth transition in time for actuators

--- source/Obstacles/CylinderNozzle.cpp
+++ source/Obstacles/CylinderNozzle.cpp
@@ -7,7 +7,6 @@
 #include "CylinderNozzle.h"
 #include "ShapeLibrary.h"
 #include "../Utils/BufferedLogger.h"
-
 using namespace cubism;
 
 void CylinderNozzle::create(const std::vector<BlockInfo>& vInfo)
@@ -31,96 +30,122 @@ void CylinderNozzle::create(const std::vector<BlockInfo>& vInfo)
         kernel(vInfo[i], b, * obstacleBlocks[vInfo[i].blockID] );
       }
   }
+}
 
+void CylinderNozzle::finalize()
+{
+  const Real transition_duration = 1.0;
+  for (size_t idx = 0 ; idx < actuators.size(); idx++)
+  {
+    Real dummy;
+    actuatorSchedulers[idx].transition (sim.time,t_change,t_change+transition_duration,actuators_prev_value[idx],actuators_next_value[idx]);
+    actuatorSchedulers[idx].gimmeValues(sim.time,actuators[idx],dummy);
+  }
+
+  //if (sim.time >= t_action_taken[curr_idx])
+  //{
+  //  std::vector<Real> a (actuators.size());
+  //  for (size_t i = 0 ; i < actuators.size(); i++)
+  //    a[i] = action_taken[curr_idx*actuators.size()+i];
+  //  act(a,0);
+  //  curr_idx++;
+  //}
+
+  const auto & vInfo = sim.vel->getBlocksInfo();
   const Real dtheta = 2*M_PI/Nactuators;
-  const Real actw = actuator_theta*radius;
   const Real Cx = centerOfMass[0];
   const Real Cy = centerOfMass[1];
   for(size_t i=0; i<vInfo.size(); i++)
   {
-      const auto & info = vInfo[i];
-      if(obstacleBlocks[info.blockID] == nullptr) continue; //obst not in block
-      ObstacleBlock& o = * obstacleBlocks[info.blockID];
-      UDEFMAT & __restrict__ UDEF = o.udef;
+    const auto & info = vInfo[i];
+    if(obstacleBlocks[info.blockID] == nullptr) continue; //obst not in block
+    ObstacleBlock& o = * obstacleBlocks[info.blockID];
+    UDEFMAT & __restrict__ UDEF = o.udef;
 
-      for(int iy=0; iy<ScalarBlock::sizeY; iy++)
-      for(int ix=0; ix<ScalarBlock::sizeX; ix++)
+    for(int iy=0; iy<ScalarBlock::sizeY; iy++)
+    for(int ix=0; ix<ScalarBlock::sizeX; ix++)
+    {
+      UDEF[iy][ix][0] = 0.0;
+      UDEF[iy][ix][1] = 0.0;
+      Real p[2];
+      info.pos(p, ix, iy);
+      const Real x = p[0]-Cx;
+      const Real y = p[1]-Cy;
+      const Real r = x*x+y*y;
+
+      if (r > (radius+2*info.h)*(radius+2*info.h) || r < (radius-2*info.h)*(radius-2*info.h)) continue;
+
+      Real theta = atan2(y,x);
+      if (theta < 0) theta += 2.*M_PI;
+
+      int idx = round(theta / dtheta); //this is the closest actuator
+      if (idx == Nactuators) idx = 0;  //periodic around the cylinder
+
+      const Real theta0 = idx * dtheta;
+
+      const Real phi = theta - theta0;
+      if ( std::fabs(phi) < 0.5*actuator_theta || (idx == 0 && std::fabs(phi-2*M_PI) < 0.5*actuator_theta))
       {
-          UDEF[iy][ix][0] = 0.0;
-          UDEF[iy][ix][1] = 0.0;
-          Real p[2];
-          info.pos(p, ix, iy);
-	  const double x = p[0]-Cx;
-	  const double y = p[1]-Cy;
-	  const double r = x*x+y*y;
-	  //if (r < 0.6*0.6*radius*radius) continue;
-	  if (r < 0.9*0.9*radius*radius) continue;
-
-	  double theta = atan2(y,x);
-	  if (theta < 0) theta += 2.*M_PI;
-
-	  int idx = round(theta / dtheta); //this is the closest actuator
-	  if (idx == Nactuators) idx = 0;  //periodic around the cylinder
-
-	  const double theta0 = idx * dtheta;
-
-	  const double phi = theta - theta0;
-          if ( std::fabs(phi) < 0.5*actuator_theta || (idx == 0 && std::fabs(phi-2*M_PI) < 0.5*actuator_theta))
-          {
-	       const double ur = 0.01*actuators[idx]/r*cos(M_PI/actw*phi);
-               UDEF[iy][ix][0] = ur * cos(theta);
-               UDEF[iy][ix][1] = ur * sin(theta);
-          }
+        const Real rr = pow(r,0.5);
+        const Real ur = 0.01*actuators[idx]/rr*cos(M_PI*phi/actuator_theta);
+        UDEF[iy][ix][0] = ur * cos(theta);
+        UDEF[iy][ix][1] = ur * sin(theta);
       }
+    }
   }
+  fx_integral += -std::fabs(forcex)*sim.dt;
 }
 
 void CylinderNozzle::act( std::vector<Real> action, const int agentID)
 {
+  t_change = sim.time;
   if(action.size() != actuators.size())
   {
     std::cerr << "action size needs to be equal to actuators\n";
     fflush(0);
     abort();
   }
-  double Q = 0;
+  Real Q = 0;
   for (size_t i = 0 ; i < action.size() ; i ++)
   {
-    actuators[i] += action[i];
-    Q += actuators[i];
+    actuators_prev_value[i] = actuators[i];
+    actuators_next_value[i] = action   [i];
+    Q += action[i];
   }
   Q /= actuators.size();
-  for (size_t i = 0 ; i < action.size() ; i ++) actuators[i] -= Q;
+  for (size_t i = 0 ; i < action.size() ; i ++) actuators_next_value[i] -= Q;
 }
 
 Real CylinderNozzle::reward(const int agentID)
 {
-  return -std::fabs(forcex);
+  Real retval = fx_integral;
+  fx_integral = 0;
+  return retval;
 }
 
 std::vector<Real> CylinderNozzle::state(const int agentID)
 {
-  std::vector<double> S;
+  std::vector<Real> S;
   S.push_back(sim.time   );
   S.push_back(forcex     );
   S.push_back(forcey     );
   const int bins = actuators.size();
-  const double dtheta = 2.*M_PI / bins;
-  std::vector<int>     n_s   (bins,0.0);
-  std::vector<double>  p_s   (bins,0.0);
-  std::vector<double> fX_s   (bins,0.0);
-  std::vector<double> fY_s   (bins,0.0);
+  const Real dtheta = 2.*M_PI / bins;
+  std::vector<int>   n_s   (bins,0.0);
+  std::vector<Real>  p_s   (bins,0.0);
+  std::vector<Real> fX_s   (bins,0.0);
+  std::vector<Real> fY_s   (bins,0.0);
   for(auto & block : obstacleBlocks) if(block not_eq nullptr)
   {
     for(size_t i=0; i<block->n_surfPoints; i++)
     {
-      const double x     = block->x_s[i] - origC[0];
-      const double y     = block->y_s[i] - origC[1];
-      const double ang   = atan2(y,x);
-      const double theta = ang < 0 ? ang + 2*M_PI : ang;
-      const double p     = block->p_s[i];
-      const double fx    = block->fX_s[i];
-      const double fy    = block->fY_s[i];
+      const Real x     = block->x_s[i] - origC[0];
+      const Real y     = block->y_s[i] - origC[1];
+      const Real ang   = atan2(y,x);
+      const Real theta = ang < 0 ? ang + 2*M_PI : ang;
+      const Real p     = block->p_s[i];
+      const Real fx    = block->fX_s[i];
+      const Real fy    = block->fY_s[i];
       const int idx = theta / dtheta;
       n_s [idx] ++;
       p_s [idx] += p;
--- source/Obstacles/CylinderNozzle.h
+++ source/Obstacles/CylinderNozzle.h
@@ -7,13 +7,22 @@
 #pragma once
 
 #include "../Shape.h"
+#include "FishUtilities.h"
 
 class CylinderNozzle : public Shape
 {
   const Real radius;
   std::vector<Real> actuators;
+  std::vector<Real> actuators_prev_value;
+  std::vector<Real> actuators_next_value;
   const int Nactuators;
   const Real actuator_theta;
+  Real fx_integral = 0;
+  //std::vector<double>   action_taken;
+  //std::vector<double> t_action_taken;
+  //int curr_idx = 0;
+  std::vector < Schedulers::ParameterSchedulerScalar > actuatorSchedulers;
+  Real t_change = 0;
 
  public:
 
@@ -22,9 +31,45 @@ class CylinderNozzle : public Shape
 		Nactuators ( p("-Nactuators").asInt(2)), actuator_theta ( p("-actuator_theta").asDouble(10.)*M_PI/180.)
   {
 	  actuators.resize(Nactuators,0.);
+    actuatorSchedulers.resize(Nactuators);
+    actuators_prev_value.resize(Nactuators);
+    actuators_next_value.resize(Nactuators);
+    #if 0
+    if (sim.rank == 0)
+    {
+      std::string line;
+      ifstream myfile;
+      myfile.open ("actions0.txt",std::ios_base::in);
+
+      while (std::getline(myfile, line))
+      {
+          std::istringstream iss(line);
+          std::cout << "--> ";
+          double temp;
+          iss >> temp;
+          t_action_taken.push_back(temp);
+          std::cout << temp << " ";
+          for( size_t a = 0; a<actuators.size(); a++ )
+          {
+            iss >> temp;
+            std::cout << temp << " ";
+            action_taken.push_back(temp);
+          }
+          std::cout << std::endl;
+      }
+      myfile.close();
+    }
+    int n = (int) t_action_taken.size();
+    MPI_Bcast(&n, 1, MPI_INT, 0, sim.comm );
+    t_action_taken.resize(n);
+    action_taken.resize(actuators.size()*n);
+    MPI_Bcast(t_action_taken.data(), t_action_taken.size(), MPI_DOUBLE, 0, sim.comm );
+    MPI_Bcast(  action_taken.data(),   action_taken.size(), MPI_DOUBLE, 0, sim.comm );
+    #endif
   }
 
   void create(const std::vector<cubism::BlockInfo>& vInfo) override;
+  void finalize() override;
 
   Real getCharLength() const override
   {
