e01d3674542311d59d29155a79a9c27d4b71add4
Michail Chatzimanolakis <mchatzim@daint102.login.cscs.ch>
2023-03-22 11:32:52 +0100

    Added heaving motion to hydrofoil (NACA), for compatibility with 3D code. Added a few comments regarding how the hydrofoil moves. Also added option to have a mean angle for pitching.

--- source/Obstacles/Naca.cpp
+++ source/Obstacles/Naca.cpp
@@ -1,6 +1,6 @@
 //
 //  CubismUP_2D
-//  Copyright (c) 2021 CSE-Lab, ETH Zurich, Switzerland.
+//  Copyright (c) 2023 CSE-Lab, ETH Zurich, Switzerland.
 //  Distributed under the terms of the MIT license.
 //
 
@@ -13,11 +13,32 @@ using namespace cubism;
 class NacaData : public FishData
 {
   const Real tRatio;
- public:
-  NacaData(Real L, Real _h, Real _tRatio)
-  : FishData(L, _h), tRatio(_tRatio) { _computeWidth(); }
 
-  void computeMidline(const Real time, const Real dt) override;
+ public:
+  NacaData(Real L, Real _h, Real _tRatio) : FishData(L, _h), tRatio(_tRatio)
+  { 
+    _computeWidth();
+  }
+
+  void computeMidline(const Real time, const Real dt) override
+  {
+    rX[0] = rY[0] = vX[0] = vY[0] = norX[0] = vNorX[0] = vNorY[0] = 0.0;
+    vNorY[0] = 1.0;
+  
+    #pragma omp parallel for schedule(static)
+    for(int i=1; i<Nm; ++i)
+    {
+      // only x-coordinate of midline varies, the rest is 0
+      const Real dx = std::fabs(rS[i]-rS[i-1]);
+      rX[i] = dx;
+      rY[i] = vX[i] = vY[i] = norX[i] = vNorX[i] = vNorY[i] = 0.0;
+      norY[i] = 1.0;
+    }
+  
+    for(int i=1; i<Nm; ++i)
+      rX[i] += rX[i-1];
+  }
+
   Real _width(const Real s, const Real L) override
   {
     const Real a =  0.2969;
@@ -26,80 +47,66 @@ class NacaData : public FishData
     const Real d =  0.2843;
     const Real e = -0.1015; // -0.1036 instead of -0.1015 to ensure closing end
     const Real t = tRatio*L; //NACA00{tRatio}
-
     if(s<0 or s>L) return 0;
     const Real p = s/L;
     const Real w = 5*t*(a*std::sqrt(p) + b*p + c*p*p + d*p*p*p + e*p*p*p*p);
-    // std::cout << "s=" << s << ", w=" << w << std::endl;
     assert( w >= 0 );
     return w;
   }
 };
 
-void NacaData::computeMidline(const Real t, const Real dt)
+Naca::Naca(SimulationData&s, ArgumentParser&p, Real C[2]): Fish(s,p,C)
 {
-  // TODO: For NACA the midline is static
-  rX[0] = rY[0] = vX[0] = vY[0] = norX[0] = vNorX[0] = vNorY[0] = 0.0;
-  vNorY[0] = 1.0;
-
-  #pragma omp parallel for schedule(static)
-  for(int i=1; i<Nm; ++i) {
-    // only x-coordinate of midline varies
-    const Real dx = std::fabs(rS[i]-rS[i-1]);
-    rX[i] = dx;
-    // rest 0
-    rY[i] = vX[i] = vY[i] = norX[i] = vNorX[i] = vNorY[i] = 0.0;
-    // thus normal is
-    norY[i] = 1.0;
-  }
-
-  for(int i=1; i<Nm; ++i){
-    rX[i] += rX[i-1];
-  }
-}
-
-Naca::Naca(SimulationData&s, ArgumentParser&p, Real C[2])
-  : Fish(s,p,C), Apitch( p("-Apitch").asDouble(0.0)*M_PI/180 ), Fpitch(p("-Fpitch").asDouble(0.0)), tAccel(p("-tAccel").asDouble(-1)), fixedCenterDist(p("-fixedCenterDist").asDouble(0))  {
-  const Real tRatio = p("-tRatio").asDouble(0.12);
-  myFish = new NacaData(length, sim.minH, tRatio);
-  if( sim.rank == 0 && s.verbose  ) printf("[CUP2D] - NacaData Nm=%d L=%f t=%f A=%f w=%f xvel=%f yvel=%f tAccel=%f fixedCenterDist=%f\n",myFish->Nm, (double)length, (double)tRatio, (double)Apitch, (double)Fpitch, (double)forcedu, (double)forcedv, (double)tAccel, (double)fixedCenterDist);
+  Apitch = p("-Apitch").asDouble(0.0)*M_PI/180; //aplitude of sinusoidal pitch angle
+  Fpitch = p("-Fpitch").asDouble(0.0)         ; //frequency
+  Mpitch = p("-Mpitch").asDouble(0.0)*M_PI/180; //mean angle
+  Fheave = p("-Fheave").asDouble(0.0)         ; //frequency of rowing motion
+  Aheave = p("-Aheave").asDouble(0.0)*length  ; //amplitude (NON DIMENSIONAL)
+  tAccel = p("-tAccel").asDouble(-1);
+  fixedCenterDist = p("-fixedCenterDist").asDouble(0);
+  const Real thickness = p("-tRatio").asDouble(0.12);
+  myFish = new NacaData(length, sim.minH, thickness);
+  if( sim.rank == 0 && sim.verbose) printf("[CUP2D] - NacaData Nm=%d L=%f t=%f A=%f w=%f xvel=%f yvel=%f tAccel=%f fixedCenterDist=%f\n",myFish->Nm, (double)length, (double)thickness, (double)Apitch, (double)Fpitch, (double)forcedu, (double)forcedv, (double)tAccel, (double)fixedCenterDist);
 }
 
 void Naca::updateVelocity(Real dt)
 {
   const Real omegaAngle = 2*M_PI*Fpitch;
-  const Real angle = Apitch*std::sin(omegaAngle*sim.time);
+  const Real angle = Mpitch + Apitch*std::sin(omegaAngle*sim.time);
   // angular velocity
   omega = Apitch*omegaAngle*std::cos(omegaAngle*sim.time);
+
+  // heaving motion
+  const Real v_heave = -2.0*M_PI*Fheave*Aheave*std::sin(2*M_PI*Fheave*sim.time);
   if( sim.time < tAccel )
   {
     // linear velocity (due to rotation-axis != CoM)
     u = (sim.time/tAccel)*forcedu - fixedCenterDist*length*omega*std::sin(angle);
-    v = (sim.time/tAccel)*forcedv + fixedCenterDist*length*omega*std::cos(angle);
+    v = (sim.time/tAccel)*forcedv + fixedCenterDist*length*omega*std::cos(angle) + v_heave;
   }
   else
   {
     // linear velocity (due to rotation-axis != CoM)
     u = forcedu - fixedCenterDist*length*omega*std::sin(angle);
-    v = forcedv + fixedCenterDist*length*omega*std::cos(angle);
+    v = forcedv + fixedCenterDist*length*omega*std::cos(angle) + v_heave;
   }
-  // std::cout << "u=" << u << ", v=" << v << "\n";
 }
 
 void Naca::updateLabVelocity( int nSum[2], Real uSum[2] )
 {
-  if(bFixedx){
+  // heaving motion
+  const Real v_heave = -2.0*M_PI*Fheave*Aheave*std::sin(2*M_PI*Fheave*sim.time);
+
+  if(bFixedx)
+  {
    (nSum[0])++; 
-   if( sim.time < tAccel )
-    uSum[0] -= (sim.time/tAccel)*forcedu;
-   else 
-    uSum[0] -= forcedu; 
+   if( sim.time < tAccel ) uSum[0] -= (sim.time/tAccel)*forcedu;
+   else                    uSum[0] -=                   forcedu; 
   }
-  if(bFixedy){
+  if(bFixedy)
+  {
    (nSum[1])++; 
-   if( sim.time < tAccel )
-    uSum[1] -= (sim.time/tAccel)*forcedv;
-   else 
-    uSum[1] -= forcedv; 
+   if( sim.time < tAccel )uSum[1] -= (sim.time/tAccel)*forcedv + v_heave;
+   else                   uSum[1] -=                   forcedv + v_heave; 
   }
 }
--- source/Obstacles/Naca.h
+++ source/Obstacles/Naca.h
@@ -1,6 +1,6 @@
 //
 //  CubismUP_2D
-//  Copyright (c) 2021 CSE-Lab, ETH Zurich, Switzerland.
+//  Copyright (c) 2023 CSE-Lab, ETH Zurich, Switzerland.
 //  Distributed under the terms of the MIT license.
 //
 
@@ -10,13 +10,32 @@
 
 class Naca: public Fish
 {
-  const Real Apitch; // aplitude of sinusoidal pitch angle
-  const Real Fpitch; // frequency
-  const Real tAccel; // time to accelerate to target velocity
-  const Real fixedCenterDist; // distance s/L from CoM where hydrofoil is fixed
+  /*
+   Hydrofoil motion is defined as:
+
+      Rotation:
+      a(t) = Mpitch + Apitch*sin(2*pi*Fpitch*t), where:
+             a(t)   :pitching angle
+             Mpitch :mean pitch angle
+             Fpitch :pitching frequency
+      omega(t) = da/dt
+      Rotation can be defined around a point that is located at a distance of
+      d = fixedCenterDist*length for the hydrofoil's center of mass.
+      In this case, we add the following velocity to the motion:
+        u_rot = - d*omega(t)*sin(a(t))
+        v_rot = + d*omega(t)*cos(a(t))
+
+      Heaving motion:
+      y(t) = Aheave*cos(2*pi*Fheave*t)
+      v(t) = dy/dt = -2.0*pi*Fheave*Aheave*sin(2*pi*Fheave*t)
+
+      It is also possible to add a constant velocity (uforced,vforced) to the motion.
+  */
+  Real Apitch, Fpitch, Mpitch, Fheave, Aheave;
+  Real tAccel; // time to accelerate to target velocity
+  Real fixedCenterDist; // distance s/L from CoM where hydrofoil is fixed
 
  public:
-
   Naca(SimulationData&s, cubism::ArgumentParser&p, Real C[2]);
   void updateVelocity(Real dt) override;
   void updateLabVelocity( int mSum[2], Real uSum[2] ) override;
