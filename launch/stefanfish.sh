a3bc26250da7b4b3df2fb99be268e731298ec976
Sergey Litvinov <slitvinov@gmail.com>
2025-03-25 21:28:21 +0100

    restore stefanfish

---
+++ launch/stefanfish.sh
@@ -0,0 +1,22 @@
+./simulation \
+    -bAdaptChiGradient 0 \
+    -bMeanConstraint 1 \
+    -bpdx 4 \
+    -bpdy 2 \
+    -CFL 0.4 \
+    -Ctol 1.0 \
+    -extent 2 \
+    -levelMax 9 \
+    -levelStart 4 \
+    -muteAll 0 \
+    -nu 0.00004 \
+    -poissonSolver iterative \
+    -poissonTol 1e-5 \
+    -poissonTolRel 0 \
+    -Rtol 2.0 \
+    -tdump 0.1 \
+    -tend 5.0 \
+    -verbose 1 \
+    -shapes '
+stefanfish L=0.2 T=1 xpos=0.6 bFixed=1
+'
