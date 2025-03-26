92bcb5e0317a79a77372fff82ce98cb2fc2a5c86
Sergey Litvinov <slitvinov@gmail.com>
2025-03-26 11:01:37 +0100

    rm old/

--- launch/old/launchCylFlow.sh
+++
@@ -1,8 +0,0 @@
-BPDX=${BPDX:-16}
-BPDY=${BPDY:-8}
-
-OPTIONS="-bpdx ${BPDX} -bpdy ${BPDY} -tdump 0.02 -nu 0.0003 -tend 80 "
-OBJECTS='disk radius=0.03 xpos=0.3 ypos=0.25 bForced=1 bFixed=1 xvel=0.2
-'
-
-source launchCommon.sh
--- launch/old/launchVariableRhoCylinder.sh
+++
@@ -1,6 +0,0 @@
-# Reynolds 1000 :
-OPTIONS="-bpdx 64 -bpdy 64 -tdump 0.05 -nu 0.0000877767 -tend 8"
-OBJECTS='diskVarDensity radius=0.05 bFixed=1 rhoTop=1.5 rhoBot=0.5
-'
-
-source launchCommon.sh
