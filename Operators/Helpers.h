5564a82c3df8ba528c122df66fbe1f80a4edaf4a
Sergey Litvinov <slitvinov@gmail.com>
2025-03-27 22:03:43 +0100

    no gaussianIC

--- Operators/Helpers.h
+++ Operators/Helpers.h
@@ -21,15 +21,6 @@ public:
   void operator()(const Real dt);
   std::string getName() { return "IC"; }
 };
-class gaussianIC : public Operator {
-protected:
-  const std::vector<cubism::BlockInfo> &velInfo = sim.vel->getBlocksInfo();
-
-public:
-  gaussianIC(SimulationData &s) : Operator(s) {}
-  void operator()(const Real dt);
-  std::string getName() { return "gaussianIC"; }
-};
 class ApplyObjVel : public Operator {
 protected:
   const std::vector<cubism::BlockInfo> &velInfo = sim.vel->getBlocksInfo();
