31cc6f1f193487d4ef34728236896aafd406b55d
Sergey Litvinov <slitvinov@gmail.com>
2025-03-28 21:33:47 +0100

    print backtrace

--- main.cpp
+++ main.cpp
@@ -3,18 +3,29 @@
 #include <fenv.h>
 #include <csignal>
 #include <iostream>
+#include <execinfo.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
 #include "Simulation.h"
 struct Config cfg;
 
-void fpe_handler(int sig) {
-  fprintf(stderr, "main: error: floating point exception\n");
-  std::abort();
+void handler(int sig)
+{
+  void *array[10];
+  size_t size;
+  size = backtrace(array, 10);
+  fprintf(stderr, "Error: signal %d:\n", sig);
+  backtrace_symbols_fd(array, size, STDERR_FILENO);
+  exit(1);
 }
 
 int main(int argc, char **argv) {
   int threadSafety, rank;
   MPI_Init_thread(&argc, &argv, MPI_THREAD_MULTIPLE, &threadSafety);
-  std::signal(SIGFPE, fpe_handler);
+  std::signal(SIGFPE, handler);
   fesetenv(FE_NOMASK_ENV);
   fedisableexcept(FE_ALL_EXCEPT);
   feenableexcept(FE_DIVBYZERO);
