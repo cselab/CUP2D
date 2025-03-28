937f0c2d124853c23c9f38137a84b5907cd25427
Sergey Litvinov <slitvinov@gmail.com>
2025-03-28 20:43:54 +0100

    up

--- main.cpp
+++ main.cpp
@@ -7,20 +7,19 @@
 struct Config cfg;
 
 void fpe_handler(int sig) {
-  fprintf(stderr, "main: error: floating point exception");
+  fprintf(stderr, "main: error: floating point exception\n");
   std::abort();
 }
 
 int main(int argc, char **argv) {
   int threadSafety, rank;
+  MPI_Init_thread(&argc, &argv, MPI_THREAD_MULTIPLE, &threadSafety);
   std::signal(SIGFPE, fpe_handler);
   fesetenv(FE_NOMASK_ENV);
   fedisableexcept(FE_ALL_EXCEPT);
   feenableexcept(FE_DIVBYZERO);
   feenableexcept(FE_INVALID);
   feenableexcept(FE_OVERFLOW);
-
-  MPI_Init_thread(&argc, &argv, MPI_THREAD_MULTIPLE, &threadSafety);
   cfg.comm = MPI_COMM_WORLD;
   MPI_Comm_rank(cfg.comm, &cfg.rank);
   MPI_Comm_rank(cfg.comm, &cfg.size);
