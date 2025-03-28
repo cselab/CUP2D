a8903bc1dd9395d82f16ccef875916b1db71c6cb
Sergey Litvinov <slitvinov@gmail.com>
2025-03-28 19:29:05 +0100

    fix view.py

--- tools/view.py
+++ tools/view.py
@@ -10,23 +10,17 @@ else:
 import paraview
 from paraview.simple import *
 
-size = 1200, 1200
 view = CreateView("RenderView")
-view.ViewSize = size
 view.OrientationAxesVisibility = 0
 view.CameraParallelProjection = 1
 view.Background = 1, 1, 1
 layout = CreateLayout()
 layout.AssignView(0, view)
-layout.SetSize(*view.ViewSize)
 xdmf = OpenDataFile(xdmf_path)
 threshold = Threshold(Input=xdmf)
 threshold.Scalars = 'CELLS', 'chi'
 threshold.UpperThreshold = 0.5
 disp = Show(threshold)
-# ColorBy(disp, ('CELLS', 'tmp'))
-
-# get color transfer function/color map for 'tmp'
 tmpLUT = GetColorTransferFunction('tmp')
 tmpLUT.RGBPoints = [-1.0, 0.176471, 0.0, 0.294118, -0.87451, 0.272434, 0.095963, 0.444214, -0.74902, 0.373395, 0.228912, 0.56932, -0.623529, 0.481661, 0.415917, 0.657901, -0.498039, 0.601922, 0.562937, 0.750481, -0.372549, 0.718493, 0.695886, 0.836986, -0.24705900000000003, 0.811995, 0.811534, 0.898501, -0.12156900000000004, 0.894733, 0.8995, 0.940023, 0.00392156999999993, 0.969166, 0.966859, 0.963629, 0.12941200000000008, 0.98639, 0.910265, 0.803691, 0.25490199999999996, 0.995002, 0.835371, 0.624375, 0.38039200000000006, 0.992541, 0.736947, 0.420146, 0.5058820000000002, 0.931949, 0.609458, 0.224221, 0.631373, 0.85075, 0.483968, 0.069819, 0.7568630000000001, 0.740023, 0.380623, 0.035371, 0.8823530000000002, 0.617993, 0.29827, 0.026759, 1.0, 0.498039, 0.231373, 0.031373]
 disp.LookupTable = tmpLUT
@@ -37,15 +31,21 @@ view.CameraParallelProjection = 1
 view.UseColorPaletteForBackground = 0
 view.Background = 1, 1, 1
 
-bnd = threshold.GetDataInformation().GetBounds()
+xl, xy, yl, yh, zl, zh = threshold.GetDataInformation().GetBounds()
 margin = 0
-cx = 0.5 * (bnd[0] + bnd[1])
-cy = 0.5 * (bnd[2] + bnd[3])
-cz = 0.5 * (bnd[4] + bnd[5])
-dx = 0.5 * (bnd[1] - bnd[0]) * (1 + margin)
-dy = 0.5 * (bnd[3] - bnd[2]) * (1 + margin)
-dz = 0.5 * (bnd[5] - bnd[4]) * (1 + margin)
+cx = 0.5 * (xl + xy)
+cy = 0.5 * (yl + yh)
+cz = 0.5 * (zl + zh)
+dx = 0.5 * (xh - xl) * (1 + margin)
+dy = 0.5 * (yh - yl) * (1 + margin)
+dz = 0.5 * (zh - zl) * (1 + margin)
+
+width = 1200
+height = int(width * dy / dx)
+view.ViewSize = width, height
+layout.SetSize(width, height)
+
 view.CameraFocalPoint = cx, cy, cz
 view.CameraPosition = cx, cy, cz + 2 * max(dx, dy, dz)
-view.CameraParallelScale = max(dx, dy)
+view.CameraParallelScale = max(dx, dy) // 2
 SaveScreenshot(png_path, view)
