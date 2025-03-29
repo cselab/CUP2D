6ad5c95c08c6cd58b689150a03e0e25d5afaa797
Sergey Litvinov <slitvinov@gmail.com>
2025-03-29 12:40:02 +0100

    better floating point exception report

--- main.cpp
+++ main.cpp
@@ -15,15 +15,12 @@ static void handler(int);
 int main(int argc, char **argv) {
   int threadSafety, rank;
   MPI_Init_thread(&argc, &argv, MPI_THREAD_MULTIPLE, &threadSafety);
-  std::signal(SIGFPE, handler);
-  fesetenv(FE_NOMASK_ENV);
-  fedisableexcept(FE_ALL_EXCEPT);
-  feenableexcept(FE_DIVBYZERO);
-  feenableexcept(FE_INVALID);
-  feenableexcept(FE_OVERFLOW);
   cfg.comm = MPI_COMM_WORLD;
   MPI_Comm_rank(cfg.comm, &cfg.rank);
   MPI_Comm_rank(cfg.comm, &cfg.size);
+  std::signal(SIGFPE, handler);
+  feclearexcept(FE_ALL_EXCEPT);
+  feenableexcept(FE_DIVBYZERO | FE_INVALID | FE_OVERFLOW);
   Simulation *sim = new Simulation(argc, argv, MPI_COMM_WORLD);
   sim->init();
   sim->simulate();
@@ -32,27 +29,13 @@ int main(int argc, char **argv) {
   return 0;
 }
 
-static const struct {
-  int sig;
-  const char *name, *desc;
-} FEInfo[] = {
-    {FE_DIVBYZERO, "FE_DIVBYZERO", "Division by zero"},
-    {FE_INEXACT, "FE_INEXACT", "Inexact result (rounding occurred)"},
-    {FE_INVALID, "FE_INVALID", "Invalid operation (e.g., sqrt(-1))"},
-    {FE_OVERFLOW, "FE_OVERFLOW", "Overflow (result too large to represent)"},
-    {FE_UNDERFLOW, "FE_UNDERFLOW", "Underflow (result too small to represent)"},
-};
 static void handler(int sig) {
   void *array[10];
   size_t size, i;
   char **strings;
-  const char *name = "unknown";
-  for (i = 0; i < sizeof FEInfo / sizeof *FEInfo; i++)
-    if (FEInfo[i].sig == sig)
-      name = FEInfo[i].name;
   size = backtrace(array, 10);
-  fprintf(stderr, "%s:%d: error: '%s' floating point exception on rank %d\n",
-          __FILE__, __LINE__, name, cfg.rank);
+  fprintf(stderr, "%s:%d: error: floating point exception on rank %d\n",
+          __FILE__, __LINE__, cfg.rank);
   size = backtrace(array, 10);
   strings = backtrace_symbols(array, size);
   if (strings != NULL) {
